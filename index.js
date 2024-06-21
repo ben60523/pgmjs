e8045075ca9402ac9c72c77e47c55e0b60ac5e42
ben60523 <37060863+ben60523@users.noreply.github.com>
2024-06-21 10:48:01 +0800

    Exports the module

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const pgmjs = require('bindings')('pgmjs');
 const math = require('mathjs')
-const buf = pgmjs.readPGM('/home/ben/map.pgm')
-console.log(math.mean(Array.from(buf)))
-console.log(math.max(Array.from(buf)))
-console.log(math.min(Array.from(buf)))
+
+module.exports = {
+  readPGM: function (fp) {
+      return pgmjs.readPGM(fp);
+  },
+}
